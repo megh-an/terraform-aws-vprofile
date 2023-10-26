167d40c41a86f1797591099ce73603d95a9cde9e
megha <anmeghan04@gmail.com>
2023-10-26 20:39:20 +0530

    providers

--- backend-s3.tf
+++ backend-s3.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "terra-vprofile-state121"
-    key = "terraform/backend"
-    region ="eu-north-1"
+    key    = "terraform/backend"
+    region = "eu-north-1"
   }
 }
 
