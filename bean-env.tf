429d5d7907729bcd16cb7bc4f2e16d6f58048966
megha <anmeghan04@gmail.com>
2023-10-26 22:06:13 +0530

    bean env

--- bean-env.tf
+++ bean-env.tf
@@ -1,6 +1,7 @@
 resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
   name                = "vprofile-bean-prod"
   application         = aws_elastic_beanstalk_application.vprofile-prod.name
+  instance_security_groups = ["sg-0123456789abcdef0"]
   solution_stack_name = "64bit Amazon Linux 2 v4.3.13 running Tomcat 8.5 Corretto 11"
   cname_prefix        = "vprofile-bean-prod-domain"
   setting {
