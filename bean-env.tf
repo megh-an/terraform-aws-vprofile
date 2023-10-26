7350d81c6eb2dc5c100dc4605d81c7476b6494e5
megha <anmeghan04@gmail.com>
2023-10-26 21:55:21 +0530

    bean env

--- bean-env.tf
+++ bean-env.tf
@@ -1,7 +1,7 @@
 resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
   name                = "vprofile-bean-prod"
   application         = aws_elastic_beanstalk_application.vprofile-prod.name
-  solution_stack_name = "64bit Amazon Linux 2023 v5.0.2 running Tomcat 10 Corretto 17"
+  solution_stack_name = "64bit Amazon Linux 2 v4.3.13 running Tomcat 8.5 Corretto 11"
   cname_prefix        = "vprofile-bean-prod-domain"
   setting {
     name      = "VPCId"
